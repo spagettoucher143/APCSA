ba23617ef3f09cf4c6a9dbd67c5b974144ebf237
nakanishid0098 <nakanishid0098@CA-SU-F106-25.sduhsd.lan>
2018-02-02 13:57:10 -0800

    Upload APCSA School Project

---
+++ APCSA/src/Unit0/Lab0b.java
@@ -0,0 +1,46 @@
+package Unit0;
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = 12345;
+		int intOne = 24;
+		long longValue = 80000000;
+
+
+		//decimal variables
+		float floatOne = 88.88888f;
+		double doubleOne = 58.463629;
+		
+
+
+		//other integer types
+		char charOne = 'A';
+		
+
+
+		//other types
+		boolean booleanOne = true;
+		String strValue = "OH BOI";
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Jim Bob                08/18/06*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - byteOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - byteOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - byteOne = "+longValue+"\t\t*");
+	}
+}
