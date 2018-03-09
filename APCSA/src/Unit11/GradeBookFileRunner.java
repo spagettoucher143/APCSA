f3062165e6bbc146cab0a6738d051b225dbb1539
nakanishid0098 <nakanishid0098@CA-SU-F106-25.sduhsd.lan>
2018-03-09 14:58:35 -0800

    oi

--- APCSA/src/Unit11/GradeBookFileRunner.java
+++ APCSA/src/Unit11/GradeBookFileRunner.java
@@ -1,5 +1,36 @@
 package Unit11;
 
-public class GradeBookFileRunner {
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
 
+import java.util.Arrays;
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.util.Arrays.*;
+import java.io.File;
+
+public class GradeBookFileRunner
+{
+ public static void main( String args[] ) throws Exception
+ {
+		out.println("Welcome to the Class Stats program!");
+		
+		Scanner file = new Scanner(new File("gradebook.dat"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}		
 }
